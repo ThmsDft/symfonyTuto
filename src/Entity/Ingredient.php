6bf6a34a753eaf85d18e5ab6e117d864f22f36c6
Thomas Dufourt <tj.dufourt@gmail.com>
2023-03-15 11:55:29 +0100

    ingredients et recettes CRUD

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -5,20 +5,21 @@ namespace App\Entity;
 use App\Repository\IngredientRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[UniqueEntity('name')]
 class Ingredient
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
     #[Assert\NotBlank()]
     #[Assert\Length(min: 2, max: 50)]
     #[ORM\Column(length: 50)]
-
     private ?string $name = null;
 
     #[ORM\Column]
@@ -79,4 +80,10 @@ class Ingredient
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
+
